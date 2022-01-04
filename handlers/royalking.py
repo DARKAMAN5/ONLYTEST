79b5c952ab9c99e80a0c6d502e7824959f09636f
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2022-01-04 13:16:53 +0530

    Create royalking.py

---
+++ handlers/royalking.py
@@ -0,0 +1,14 @@
+@Client.on_message(filters.command(["join"], prefixes=f"{HNDLR}"))
+@authorized_users_only
+async def join(client, message):
+    chat_id = message.chat.id
+    try:
+        link = await client.export_chat_invite_link(chat_id)
+    except BaseException:
+        await message.reply("**Error:**\nAdd me as admin of your group!")
+        return
+    try:
+        await USER.join_chat(link)
+        await message.reply("**Userbot Joined**")
+    except UserAlreadyParticipant:
+        await message.reply("**Userbot Already Participant**")
